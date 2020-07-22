47eba71b3835247a60e728b88bd64c2b9b1a09e4
Luiz dos reis <luizdosreis@gmail.com>
2020-07-21 22:17:48 -0300

    first commit

---
+++ src/main/java/br/com/creditcardcontrol/CreditCardControlApplication.java
@@ -0,0 +1,13 @@
+package br.com.creditcardcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardControlApplication.class, args);
+	}
+
+}
